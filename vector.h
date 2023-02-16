d954bab4f436ad05ee43a664a5253faa19d6824d
CyanSquid <112185603+CyanSquid@users.noreply.github.com>
2023-02-16 19:49:56 +0000

    Update vector.h

--- vector.h
+++ vector.h
@@ -5,16 +5,16 @@
 #define vector(TYPE) TYPE*
 
 #define vector_delete(VECTOR) \
-    vector_internal_delete(&(VECTOR))
+    vector_internal_delete((void**)&(VECTOR))
 
 #define vector_push(VECTOR, DATA) \
-    (vector_internal_push(&(VECTOR), sizeof(*(VECTOR))), (VECTOR)[vector_internal_size(&(VECTOR), sizeof(*(VECTOR))) - 1] = (DATA))
+    (vector_internal_push((void**)&(VECTOR), sizeof(*(VECTOR))), (VECTOR)[vector_internal_size((void**)&(VECTOR), sizeof(*(VECTOR))) - 1] = (DATA))
 
 #define vector_size(VECTOR) \
-    vector_internal_size(&(VECTOR), sizeof(*(VECTOR)))
+    vector_internal_size((void**)&(VECTOR), sizeof(*(VECTOR)))
 
 #define vector_capacity(VECTOR) \
-    vector_internal_capacity(&(VECTOR), sizeof(*(VECTOR)))
+    vector_internal_capacity((void**)&(VECTOR), sizeof(*(VECTOR)))
 
 
 void* vector_create(void);
